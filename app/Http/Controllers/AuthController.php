d40d05b4b51774828e53fb84f63bad8b8376eeb5
salma hamdi <saalmahm51@gmail.com>
2025-03-12 22:17:34 +0000

    Réaliser login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
     public function register(Request $request){
+        
         $request->validate([
             "name"=>"required|string|max:300",
             "email"=>"required|string|max:300|email|unique:users",
@@ -22,5 +23,30 @@ class AuthController extends Controller
         ]);
 
         return response()->json(['message'=>'nod 3la slaamtk'],201);
+    }
+
+    public function login(Request $request){
+
+        $request->validate([
+            "email"=>"required|string|max:300|email|unique:users",
+            "password"=>"required|min:4"
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+        return response()->json([
+            'message' => 'Invalid credentials',
+        ], 401); 
+    }
+        $user = $request->user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+
+        return response()->json([
+            'message' => 'Login successful',
+            'token' => $token,
+        ]);
+
+
+
     }
 }
